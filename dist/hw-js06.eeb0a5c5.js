b81f7501e8648c9bf029f223229d974eea188b15
desie2610 <moiseenkodanil2013@gmail.com>
2025-11-09 15:34:43 +0200

    psh2

---
+++ dist/hw-js06.eeb0a5c5.js
@@ -0,0 +1,2 @@
+var e;let t,n=document.getElementById("app"),d=(e=()=>{let e;(e=document.createElement("p")).textContent="Click",n.appendChild(e)},(t=document.createElement("button")).textContent="Натисни мене",t.addEventListener("click",e),t);n.appendChild(d);
+//# sourceMappingURL=hw-js06.eeb0a5c5.js.map
